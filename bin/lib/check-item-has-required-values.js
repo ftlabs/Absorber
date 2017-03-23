be3f7302d152332f83b493ace2053cf31ee6eb32
Sean Tracey <seanmtracey@gmail.com>
2017-03-23 16:51:37 +0000

    Force feed items to have certain values.

---
+++ bin/lib/check-item-has-required-values.js
@@ -0,0 +1,27 @@
+const debug = require('debug')('bin:lib:check-item-has-required-values');
+const requiredItemValues = ['title', 'link', 'description', 'pubdate', 'guid'];
+
+module.exports = function(item){
+
+	return new Promise( (resolve, reject) => {
+
+		const missingRequiredItemValues = [];
+
+		requiredItemValues.forEach(key => {
+
+			if(item[key] === undefined){
+				missingRequiredItemValues.push(key);
+			}
+
+			if(missingRequiredItemValues.length > 0){
+				reject(`Item is missing the required values '${missingRequiredItemValues.join(', ')}'.`);
+			} else {
+				debug(`ITEM HAS REQUIRED FIELDS`);
+				resolve(item);
+			}
+		
+		});
+
+	});
+
+}
---
+++ bin/lib/extract-items-from-feed.js
@@ -0,0 +1,102 @@
+const debug = require('debug')('bin:lib:extract-items-from-feed');
+const fetch = require('node-fetch');
+
+const extractUUID = require('./extract-uuid');
+const parseRSSFeed = require('./parse-rss-feed');
+const separateQueryParams = require('./separate-query-parameters');
+const checkItemHasRequiredFields = require('./check-item-has-required-values');
+const checkItemHasRequiredMetadata = require('./check-metadata-has-required-values');
+
+function unpack(value){
+
+	if(value.constructor === Array){
+		return value[0];
+	} else {
+		return value;
+	}
+
+}
+
+module.exports = function(feedURL){
+	
+	return fetch(feedURL)
+		.then(res => res.text())
+		.then(text => parseRSSFeed(text))
+		.then(feed => {
+			debug(feed);
+			const P = feed.channel[0].item.map(item => {
+
+				return extractUUID( item['guid'][0]._ )
+					.then(itemUUID => {
+
+						if(itemUUID === undefined){
+							return false;
+						}
+
+						return checkItemHasRequiredFields(item)
+							.then(function(){
+
+								const audioURL = item.enclosure !== undefined ? item.enclosure[0]['$'].url : item.link[0];
+								const metadata = separateQueryParams(audioURL);
+								
+								return checkItemHasRequiredMetadata(metadata)
+									.then(function(){
+
+										debug(itemUUID, metadata);
+
+										const tableEntry = {
+											title : unpack(item.title),
+											link : unpack(item.link),
+											description : unpack(item.description),
+											pubdate : item.pubDate !== undefined ? unpack(item.pubDate) : unpack(item.pubdate),
+											guid : unpack(item.guid),
+											duration : unpack(metadata.duration),
+											uuid : unpack(metadata.uuid),
+											format : unpack(metadata.format)
+										};
+
+										tableEntry['narrator-id'] = metadata['narrator-id'];
+										tableEntry['is-human'] = metadata['is-human'];
+
+										return {
+											item,
+											metadata,
+											tableEntry,
+											audioURL
+										};
+
+									})
+									.catch(err => {
+										debug(err);
+									})
+								;
+
+
+
+							})
+							.catch(err => {
+								debug(err);
+							})
+						;
+
+					})
+				;
+				
+			});
+
+			return Promise.all(P)
+				.then(function(p){
+					debug('All done');
+					return p;
+				})
+			;
+
+		})
+		.catch(err => {
+			debug(err);
+		})
+	;
+
+
+
+}
